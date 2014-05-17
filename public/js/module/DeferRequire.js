f12dcb04e20b70fd83c1851c919de65870f1544a
람츠 <82g74nx0bm69@naver.com>
2014-05-18 07:22:22 +0900

    재시작.. 후우ㅠㅠ

--- public/js/module/DeferRequire.js
+++
@@ -1,9 +0,0 @@
-define(['Defer'], function (Defer) {
-    return function (paths) {
-        var def = new Defer();
-        require(paths, function () {
-            def.resolve(arguments);
-        });
-        return def.promise;
-    }
-});
